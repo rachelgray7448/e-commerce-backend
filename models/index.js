318ab6db3f66419c73530ba663138b3710cfdffc
Rachel Gray <rachel.gray7448@gmail.com>
2022-05-06 12:08:52 -0400

    created associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'group',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'items',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
